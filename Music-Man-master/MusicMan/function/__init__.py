62dafe3cede8b7d5c67d6d09c3931d46f9b41321
v18plus <86728159+v18plus@users.noreply.github.com>
2021-08-12 21:27:46 +0800

    Delete Music-Man-master directory

--- Music-Man-master/MusicMan/function/__init__.py
+++
@@ -1,5 +0,0 @@
-from MusicMan.function.admins import admins
-from MusicMan.function.admins import get
-from MusicMan.function.admins import set
-
-__all__ = ["set", "get", "admins"]
--- Music-Man-master/MusicMan/services/callsmusic/__init__.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client
-
-from MusicMan import config
-
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-run = client.run
