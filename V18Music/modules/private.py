7a5a798a969bedc24b4e12aaf1abfde7ac9f4f3b
v18plus <86728159+v18plus@users.noreply.github.com>
2021-08-18 03:43:06 +0800

    Add files via upload

--- V18Music/modules/private.py
+++ V18Music/modules/private.py
@@ -73,7 +73,7 @@ Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
                         "⛑ Group Support", url=f"https://t.me/{SUPPORT_GROUP}")
                 ],[
                     InlineKeyboardButton(
-                        "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
+                        "🛠 OWNER 🛠", url=f"https://{SOURCE_CODE}")
                 ]
             ]
         ),
